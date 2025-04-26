768c46989f7d5ada7ac4fed9ce2a83d18964e910
Mohammad Abdul-Shafi <mohamedsadik763@gmail.com>
2025-04-26 04:51:28 +0300

    half home screen

---
+++ lib/widgets/history_container.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:pharmascan/models/history_model.dart';
+import 'package:pharmascan/utils/app_colors.dart';
+import 'package:pharmascan/utils/app_text_styles.dart';
+
+class HistoryContainer extends StatelessWidget {
+  HistoryContainer({super.key, required this.historyModel});
+
+  HistoryModel historyModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      width: 210,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Color(0xFFBBB8B8), width: 2),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(Icons.calendar_month, color: AppColors.orange, size: 20),
+                const SizedBox(width: 5),
+                Text(
+                  historyModel.orderDate,
+                  style: AppTextStyles.text.copyWith(
+                    fontSize: 11,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  width: 60,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color:
+                        historyModel.orderStatus
+                            ? AppColors.green
+                            : AppColors.red,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: Text(
+                      historyModel.orderStatus ? 'Delivered' : 'Canceled',
+                      style: AppTextStyles.text.copyWith(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.shopping_bag_outlined,
+                  color: AppColors.orange,
+                  size: 20,
+                ),
+                SizedBox(width: 6),
+                Text(
+                  historyModel.orderId,
+                  style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Bill: ${historyModel.orderBill} EGP',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(width: 10),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
